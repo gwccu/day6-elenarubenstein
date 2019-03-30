633a178a67f5c5effbbf9ea35216974ea8174915
elenarubenstein <elenarubenstein1@gmail.com>
2019-03-30 15:17:01 +0000

    finished warmup

--- warmupDay6.py
+++ warmupDay6.py
@@ -1,1 +1,19 @@
 # File name: warmupDay6.py
+myNum = 7
+guess = int(input("Guess the magic number "))
+
+while guess != myNum:
+    guess = int(input("Guess again "))
+print("You guessed correctly.")
+
+n = 2
+m = 8
+print(n**m)
+ans = n**m
+while n != ans:
+    n*=n
+print(n)
+x = 10
+while x >= 0:
+    print (x)
+    x -= 1
